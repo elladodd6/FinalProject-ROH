2027330e6774e2f4648503af0f8069e081000ad6
elladodd6 <78565434+elladodd6@users.noreply.github.com>
2021-04-22 14:33:00 -0400

    Create map_holstein.R

---
+++ map_holstein.R
@@ -0,0 +1,10 @@
+#R script that can be run on Xanadu using 00map.sh file. Prepares .map file in the format required by PLINK software.
+
+map_hol<-read.table('cattle__732993variants__60individuals.map', header = F)
+map_hol<-data.frame(seq(1:51998), map_hol)
+names(map_hol)<-c('N','chr','SNP','Distance','Position')
+map_hol<-map_hol[,c(2,1,5)]
+map_hol$zero<-0
+map_hol<-map_hol[,c(1,2,4,3)]
+names(map_hol)<-c('Chromosome','Name','Distance','Position')
+write.table(map_hol,"holstein.map", quote = F, sep= " ", row.names = FALSE, col.names = FALSE)
